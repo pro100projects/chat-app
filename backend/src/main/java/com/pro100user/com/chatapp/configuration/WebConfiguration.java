7f2d6ac22404f3b37111fcea95e6486d89e29135
pro100user <bt769271@gmail.com>
2025-01-12 17:51:15 +0200

    feat: add web mvc configuration

---
+++ backend/src/main/java/com/pro100user/com/chatapp/configuration/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.pro100user.com.chatapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull final CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+    }
+}
